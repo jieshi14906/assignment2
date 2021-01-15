12136371f58d7209f48c1f4d8bf31f66e28a31b4
JIE SHI <20091665@mail.wit.ie>
2021-01-15 09:51:28 +0000

    Init Project Structure

---
+++ index.js
@@ -0,0 +1,47 @@
+import session from 'express-session';
+import passport from './authenticate';
+import './db';
+import dotenv from 'dotenv';
+import express from 'express';
+import moviesRouter from './api/movies';
+import bodyParser from 'body-parser';
+import {loadUsers, loadMovies} from './seedData';
+import usersRouter from './api/users';
+
+
+dotenv.config();
+const errHandler = (err, req, res, next) => {
+  /* if the error in development then send stack trace to display whole error,
+  if it's in production then just send error message  */
+  if(process.env.NODE_ENV === 'production') {
+    return res.status(500).send(`Something went wrong!`);
+  }
+  res.status(500).send(`Hey!! You caught the error 👍👍, ${err.stack} `);
+};
+if (process.env.SEED_DB) {
+  loadUsers();
+  loadMovies();
+}
+const app = express();
+
+const port = process.env.PORT;
+
+app.use(session({
+  secret: 'ilikecake',
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use('/api/movies', passport.authenticate('jwt', {session: false}), moviesRouter);
+app.use('/api/users', usersRouter);
+app.use(errHandler);
+
+
+
+app.listen(port, () => {
+  console.info(`Server running at ${port}`);
+});
