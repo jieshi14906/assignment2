ab161a28784279027842cf1266df2bb3cf51b777
JIE SHI <20091665@mail.wit.ie>
2021-01-15 12:01:48 +0000

    Use server.js as entrance file

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import './db';
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
+import genresRouter from './api/genres';
 import bodyParser from 'body-parser';
 import {loadUsers, loadMovies} from './seedData';
 import usersRouter from './api/users';
@@ -37,11 +38,8 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use('/api/movies', passport.authenticate('jwt', {session: false}), moviesRouter);
+app.use('/api/genres', genresRouter);
 app.use('/api/users', usersRouter);
 app.use(errHandler);
-
-app.listen(port, () => {
-  console.info(`Server running at ${port}`);
-});
 
 export default app;
---
+++ server.js
@@ -0,0 +1,4 @@
+import app from './index';
+
+const port = process.env.PORT;
+app.listen(port, '0.0.0.0');
